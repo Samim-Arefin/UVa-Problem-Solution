0a009c881c11c6821e612c4b78f17f9e86e6645c
Samim Arefin <67612178+Samim-Arefin@users.noreply.github.com>
2022-07-04 23:13:29 +0600

    Update Uva-900 - Brick Wall Patterns.cpp
    Updated

--- Uva-900 - Brick Wall Patterns.cpp
+++ Uva-900 - Brick Wall Patterns.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-	long long arr[50005],num,i;
-	while (cin >> num)
-	{
+#define Max 55
+long long arr[Max];
+
+void calculate(){
 		arr[0] = 1;
 		arr[1] = 1;
-		for (i = 2; i <num; i++)
+		for (int i=2; i<Max; i++)
 		{
-			arr[i]= arr[i - 1] + arr[i - 2];
+			arr[i]=arr[i-1] + arr[i-2];
 		}
-		cout<<"The Fibonacci number for "<<num<<" is "<<arr[num-1]<<endl;
+}
+int main()
+{
+	calculate();
+	int num;
+	while (std::cin >> num,num>0)
+	{
+		std::cout<<arr[num]<<'\n';
 	}
-return 0;
 }
